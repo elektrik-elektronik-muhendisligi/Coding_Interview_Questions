bb7fa85ab7e1b54760aa77ce0be48e23d036a6f2
Cagri Esen <esencgr@gmail.com>
2019-06-21 15:43:40 +0300

    add function

--- cs_tech.cpp
+++ cs_tech.cpp
@@ -8,7 +8,10 @@ using namespace std;
 
 int rand_number( void );
 bool is_valid( int );
-void compare( int, int );
+void move_to_array( int, int, int[], int[] );
+void same_digit( int[], int[], int[] );
+void diff_digit( int [], int [], int [] );
+void print( int[] );
 
 int main(){
 
@@ -16,6 +19,8 @@ int main(){
 
     cout << "---------------------GENERATING INITIAL VALUES ---------------------- \n";
     int user1, software1, user2, software2;
+    int u1[ 4 ] = { 0 }, s1[ 4 ] = { 0 }, u2[ 4 ] = { 0 }, s2[ 4 ] = { 0 };
+    int comp1[ 4 ] = { 0 }, comp2[ 4 ] = { 0 };
     cout << "\n Computer is generating a number for user..\n";
     do{
         software1 = rand_number();
@@ -37,7 +42,15 @@ int main(){
         } while ( !is_valid ( user1) );
 
         cout << " Users information code : ";
-        compare( software1, user1 );
+        move_to_array( software1, user1 , s1, u1 );
+        diff_digit( s1, u1, comp1 );
+        same_digit( s1, u1, comp1 );
+        print ( comp1 );
+
+        cout << " \n software1 : ";
+        print( s1 );
+        cout << " \n user1 : ";
+        print( u1 );
 
         if ( software1 == user1 ){
            cout << "\n\n ------ THE NUMBER IS => " << user1 << " ------> " << " USER WIN THE GAME!"<< endl;
@@ -51,7 +64,15 @@ int main(){
         } while (!is_valid ( software2 ));
 
         cout << " Computers information code :";
-        compare( software2, user2 );
+        move_to_array( software2, user2 , s2, u2 );
+        diff_digit( u2, s2, comp2 );
+        same_digit( s2, u2, comp2 );
+        print ( comp2 );
+
+        cout << " \n software2 : ";
+        print( s2 );
+        cout << " \n user2: ";
+        print( u2 );
 
         if ( software2 == user2 ){
            cout << "\n\n ------ THE NUMBER IS => " << software2 << " ------> " << " COMPUTER WIN THE GAME!"<< endl;
@@ -87,16 +108,33 @@ bool is_valid(int number){
    }
    return 1;
 }
-void compare( int num1, int num2 ){
-     int arr[ 4 ] = { 0 };
+
+void move_to_array( int num1, int num2, int n1[], int n2[] ){
+    for (int i = 3; i >= 0;  i--) {
+        int x = pow( 10, i );
+        n1[ i ] = num1 / x;
+        n2[ i ] = num2 / x;
+        num1 = num1 % x;
+        num2 = num2 % x;
+    }
+}
+
+void same_digit( int num1[], int num2[], int comp[] ){
      for (int i = 3; i >= 0;  i--) {
-         int x = pow( 10, i );
-         int temp1 = num1 / x;
-         int temp2 = num2 / x;
-             if ( temp1 == temp2 )
-                arr [ i ] = 1;
-         num1 = num1 % x;
-         num2 = num2 % x;
-     cout << arr[ i ];
+             if ( num1[ i ] == num2[ i ] )
+                comp[ i ] = 1;
      }
+}
+void diff_digit( int num2[], int num1[], int diff[] ){
+     for (int i = 3; i >= 0;  i--){
+         for (int j = 3; j >= 0; j--) {
+             if ( num1[ i ] == num2[ j ] )
+                diff[ i ] = -1;
+         }
+     }
+}
+
+void print( int arr[] ){
+    for (int i = 3;  i >= 0;  i--)
+        cout << arr[ i ];
 }
