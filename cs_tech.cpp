c3826fb8bb7772a7d0b557cc150045bef41f8fdd
Cagri Esen <esencgr@gmail.com>
2019-06-21 23:33:45 +0300

    update read.md

--- cs_tech.cpp
+++ cs_tech.cpp
@@ -1,4 +1,5 @@
 // CS-TECH APPLICANT ASSESMENT TEST 1
+// SAYI TAHMİN OYUNU
 #include <iostream>
 #include <ctime>
 #include <cstdlib>
@@ -16,7 +17,11 @@ void print( int[] );
 int main(){
 
     srand(time ( 0 ));
-
+    /*
+    Bu bölümde tahmin edilecek olan başlangıç değerleri bilgisayar ve kullanıcı
+    tarafından oluşturulur. Sayının rakamlarının tekrar etmesi durumunda sayının
+    tekrar girilmesi sağlanır.
+    */
     cout << "---------------------GENERATING INITIAL VALUES ---------------------- \n";
     int user1, software1, user2, software2;
     int u1[ 4 ] = { 0 }, s1[ 4 ] = { 0 }, u2[ 4 ] = { 0 }, s2[ 4 ] = { 0 };
@@ -31,48 +36,42 @@ int main(){
         cout << " User's generated number : ";
         cin >> user2;
     } while (!is_valid ( user2 ));
-
+    /*
+    Bu bölümde kullanıcı ve yazılım, iki taraftan biri doğru tahmin edene kadar.
+    tahminlerde bulunur. Sayının rakamlarının tekrar etmesi durumunda sayının
+    tekrar girilmesi sağlanır.
+    */
     cout << "\n\n ------------------- GUESSING & INFORMATION --------------------- ";
     while ( 1 ){
 
-        cout << "\n\n User is guessing computers number..\n";
+        cout << "\n\n User is guessing computer's number..\n";
         do{
            cout << " User's valid number : ";
            cin >> user1;
         } while ( !is_valid ( user1) );
 
-        cout << " Users information code : ";
+        cout << " User's information : ";
         move_to_array( software1, user1 , s1, u1 );
         diff_digit( s1, u1, comp1 );
         same_digit( s1, u1, comp1 );
         print ( comp1 );
-
-        cout << " \n software1 : ";
-        print( s1 );
-        cout << " \n user1 : ";
-        print( u1 );
 
         if ( software1 == user1 ){
            cout << "\n\n ------ THE NUMBER IS => " << user1 << " ------> " << " USER WIN THE GAME!"<< endl;
            break;
         }
 
-        cout << "\n\n Computer is guessing users number..\n";
+        cout << "\n\n Computer is guessing user's number..\n";
         do{
             software2 = rand_number();
-            cout << " Computer valid number : " << software2 << endl;
+            cout << " Computer's valid number : " << software2 << endl;
         } while (!is_valid ( software2 ));
 
-        cout << " Computers information code :";
+        cout << " Computers information :";
         move_to_array( software2, user2 , s2, u2 );
         diff_digit( u2, s2, comp2 );
         same_digit( s2, u2, comp2 );
         print ( comp2 );
-
-        cout << " \n software2 : ";
-        print( s2 );
-        cout << " \n user2: ";
-        print( u2 );
 
         if ( software2 == user2 ){
            cout << "\n\n ------ THE NUMBER IS => " << software2 << " ------> " << " COMPUTER WIN THE GAME!"<< endl;
@@ -83,15 +82,14 @@ int main(){
 }
 
 /*
-Function that generates random numbers between 1000 and 9999
+1000 ile 9999 arasında rasgele sayı üreten fonksiyon
 */
 int rand_number(void){
    return rand() % 9000 + 1000;
 }
 
 /*
-This test function is the check whether the number sent to it is a conditional
-number that does not repeat steps. If it is valid returned 1 value, 0  if not .
+Üretilen sayının basamaklarının birbirinden farklı olup olmadığını test eden fonksiyon
 */
 bool is_valid(int number){
    int temp, i, j;
@@ -109,6 +107,9 @@ bool is_valid(int number){
    return 1;
 }
 
+/*
+Dört basamaklı sayıların rakamlarını 4 boyutlu bir diziye atayan fonksiyon
+*/
 void move_to_array( int num1, int num2, int n1[], int n2[] ){
     for (int i = 3; i >= 0;  i--) {
         int x = pow( 10, i );
@@ -118,13 +119,20 @@ void move_to_array( int num1, int num2, int n1[], int n2[] ){
         num2 = num2 % x;
     }
 }
-
+/*
+Basamaklardan oluşturulan dizilerin karşılaştırılıp aynı değer ve aynı
+basamak değeri varsa 1 yok ise 0 değerini atayan fonksiyon
+*/
 void same_digit( int num1[], int num2[], int comp[] ){
      for (int i = 3; i >= 0;  i--) {
              if ( num1[ i ] == num2[ i ] )
                 comp[ i ] = 1;
      }
 }
+/*
+İki sayı içinde oluşturulan dizilerin karşılaştırılıp değeri doğru olan ancak
+yanlış basamakta bulunanların değerine -1 atayan fonksiyon
+*/
 void diff_digit( int num2[], int num1[], int diff[] ){
      for (int i = 3; i >= 0;  i--){
          for (int j = 3; j >= 0; j--) {
@@ -133,7 +141,9 @@ void diff_digit( int num2[], int num1[], int diff[] ){
          }
      }
 }
-
+/*
+Parametre olarak aldığı diziyi yazdıran fonksiyon
+*/
 void print( int arr[] ){
     for (int i = 3;  i >= 0;  i--)
         cout << arr[ i ];
