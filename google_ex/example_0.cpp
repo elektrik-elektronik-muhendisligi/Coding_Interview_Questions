93c05742007f198897fe5e09e7f46527960da114
Cagri Esen <esencgr@gmail.com>
2019-07-06 13:50:37 +0300

    add file

--- google_ex/example_0.cpp
+++ google_ex/example_0.cpp
@@ -1,50 +1,29 @@
-// Google coding interview questions.Daily Coding Problem: Problem #18 [Hard]
+//Google coding interview questions. Daily Coding Problem: Problem #1 [Easy]
+/* Good morning! Here's your coding interview problem for today.
+   This problem was recently asked by Google.
+   Given a list of numbers and a number k, return whether any two numbers from the list add up to k.
 
-/* Good morning! Here's your coding interview problem for today.This problem was asked by Google.
-   Given an array of integers and a number k, where 1 <= k <= length of the array, compute the maximum
-   values of each subarray of length k.
+   For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
 
-   For example, given array = [10, 5, 2, 7, 8, 7] and k = 3, we should get: [10, 7, 8, 8], since:
-
-   10 = max(10, 5, 2)
-   7 = max(5, 2, 7)
-   8 = max(2, 7, 8)
-   8 = max(7, 8, 7)
-
-   Do this in O(n) time and O(k) space. You can modify the input array in-place and you do not need to store
-   the results. You can simply print them out as you compute them.
+   Bonus: Can you do this in one pass?
 */
-
 #include <iostream>
 using namespace std;
 
-int max( int arr[ 3 ] ){
-    int max = arr[ 0 ];
-    for ( int i = 0; i < 3; i++) {
-        if ( arr[ i ] > max ){
-            max = arr[ i ];
-        }
-    }
-    return max;
-}
-
-void print( int arr[ 3 ] ){
-    for ( int i = 0; i < 3; i++) {
-        cout << arr[ i ] << " ";
-    }
-}
-
 int main(){
-    int array[ 6 ] = { 9, 5, 2, 7, 8, 7 }, top[ 3 ] = { 0 };
+    int array[ 4 ] = { 10, 15, 3, 7 }, sum = 0, ind = 0, k = 17;
+    int i, j, count = 0;
 
-    for ( int i = 0; i < 4; i++ ){
-        int x = 0;
-        for ( int j = i; j <= i+2; j++){
-            top[ x ] = array[ j ];
-            x++;
-        }
-        print( top );
-        cout << "  max --> " << max ( top ) << endl;
-    }
+            for ( i = 0; i < 3; i++) {
+                for ( j = i+1 ; j <= 3; j++) {
+                    sum = array[ i ] + array[ j ] ;
+                    cout << sum << endl;
+                    if (sum == k) {
+                        cout << array[ i ] << " + " << array[ j ] << " = " << sum << endl;
+                    break;
+                    }
+                }
+            }
+
     return 0;
 }
